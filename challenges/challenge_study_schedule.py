fcb32fb5f3ef73fc7cf7520c735ae68172c25a69
Lutti <lutti.c.amaral@gmail.com>
2023-07-26 23:07:24 -0300

    Requisito 1 - com validacoes

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -6,6 +6,13 @@ def study_schedule(permanence_period, target_time):
         return None
 
     for student_session in permanence_period:
+        if not all((
+            isinstance(student_session, tuple),
+            isinstance(student_session[0], int),
+            isinstance(student_session[1], int)
+        )):
+            return None
         if student_session[0] <= target_time <= student_session[1]:
             number_of_students += 1
+
     return number_of_students
