d47e78ad5762affa257b2eaa21072fad4f26eb6f
Lutti <lutti.c.amaral@gmail.com>
2023-07-27 00:23:00 -0300

    Requisito 2 - primeira tentativa

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("hello world", 3) == "leh_dlrow ol"
+    assert encrypt_message("hello world", 5) == "olleh_dlrow "
+
+    assert encrypt_message("hello world", 13) == "dlrow olleh"
+
+    with pytest.raises(TypeError):
+        encrypt_message("hello world", "invalid_key")
+    with pytest.raises(TypeError):
+        encrypt_message(123, 3)
