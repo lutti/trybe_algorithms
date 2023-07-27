4da40a2d69d41d7f03c7485bfd4abd452176314a
Lutti <lutti.c.amaral@gmail.com>
2023-07-27 16:30:11 -0300

    Palindromes Functions iterative and recursive

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,17 @@
 def is_palindrome_iterative(word):
     """Faça o código aqui."""
-    raise NotImplementedError
+    if word == "":
+        return False
+
+    length = len(word)
+    first = 0
+    last = length - 1
+    it_is = True
+    while first < last:
+        if word[first] == word[last]:
+            first = first + 1
+            last = last - 1
+        else:
+            it_is = False
+            break
+    return it_is
