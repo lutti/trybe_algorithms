6f5067cb28a9514aaa0874fc46f849e0d810ae81
Lutti <lutti.c.amaral@gmail.com>
2023-07-26 22:56:19 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui Lutti."""
-    raise NotImplementedError
+    number_of_students = 0
+
+    if target_time < 0 or target_time >= 24 or target_time is None:
+        return None
+
+    for student_session in permanence_period:
+        if student_session[0] <= target_time <= student_session[1]:
+            number_of_students += 1
+    return number_of_students
